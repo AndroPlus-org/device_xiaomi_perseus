e5a789341042ffe364d701f8d7c3cef5aff858c2
AndroPlus <mail@andro.plus>
2019-01-14 11:22:22 +0900

    Fix OUT_DIR

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,7 +110,7 @@ TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
-TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT}/system/lib64/android.hardware.boot@1.0.so
+TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT_DIR}/system/lib64/android.hardware.boot@1.0.so
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_USE_QCOM_HAPTICS_VIBRATOR := true
 
