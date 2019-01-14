764286801f5c46805a826dc8c61160b73cf6284e
AndroPlus <mail@andro.plus>
2019-01-14 11:44:39 +0900

    Fix include

--- omni_perseus.mk
+++ omni_perseus.mk
@@ -1,8 +1,7 @@
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) # If you are building for a phone
+$(call inherit-product, build/target/product/embedded.mk)
 
-# Inherit from those products. Most specific first.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_perseus
