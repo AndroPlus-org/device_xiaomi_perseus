71a1df118596324c3319bbc39fb84e80dfce937b
AndroPlus <mail@andro.plus>
2019-01-14 11:59:04 +0900

    Add skip_override

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,9 @@ TARGET_USES_UEFI := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := nsole=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive androidboot.fastboot=1
+# skip_override needs patch in kernel
+# https://github.com/AndroPlus-org/android_kernel_xiaomi_sdm845/commit/51387d510ce1f05e5c15729ede515eb7c8e51bfb
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive skip_override androidboot.fastboot=1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
