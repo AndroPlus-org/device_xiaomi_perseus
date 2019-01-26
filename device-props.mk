f40f5fa0b015469447fa114cb7405331b81cd2bc
AndroPlus <mail@andro.plus>
2019-01-26 21:21:19 +0900

    perseus: Add missing stock camera prop

--- device-props.mk
+++ device-props.mk
@@ -3,7 +3,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.dist=45 \
     persist.camera.sat.fallback.dist.d=5 \
     persist.camera.sat.fallback.luxindex=310 \
-    persist.camera.sat.fallback.lux.d=50
+    persist.camera.sat.fallback.lux.d=50 \
+    persist.vendor.camera.perfcapture=1
 
 # Display features
 PRODUCT_PROPERTY_OVERRIDES += \
