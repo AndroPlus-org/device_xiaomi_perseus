32bd84026fe990724e04cbfc1702637cab060d25
AndroPlus <mail@andro.plus>
2019-01-27 20:41:14 +0900

    AEX-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_perseus.mk
+    $(LOCAL_DIR)/aosp_perseus.mk
--- aosp_perseus.mk
+++ aosp_perseus.mk
@@ -6,16 +6,14 @@
 
 $(call inherit-product, device/xiaomi/perseus/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-# Define as phone to select Google Dialer
-IS_PHONE := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_perseus
+PRODUCT_NAME := aosp_perseus
 PRODUCT_DEVICE := perseus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 3
@@ -26,6 +24,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="perseus"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# GApps
-include vendor/gapps/config.mk
