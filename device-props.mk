247e98bea0783e03f8a3d53b0b73b70649991297
AndroPlus <mail@andro.plus>
2019-02-02 11:51:06 +0900

    perseus: Add missing props

--- device-props.mk
+++ device-props.mk
@@ -6,6 +6,49 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.lux.d=50 \
     persist.vendor.camera.perfcapture=1
 
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    tunnel.audio.encode=true \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-scan=true \
+    media.stagefright.audio.deep=true \
+    mmp.enable.3g2=true \
+    media.aac_51_output_enabled=true \
+    mm.enable.smoothstreaming=true \
+    vendor.mm.enable.qcom_parser=16776191 \
+    persist.mm.enable.prefetch=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    bt.max.hfpclient.connections=1 \
+    persist.vendor.btstack.enable.splita2dp=true \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.btstack.enable.twsplus=true \
+    persist.vendor.btstack.connect.peer_earbud=true \
+    ro.bluetooth.emb_wp_mode=false \
+    ro.bluetooth.wipower=false
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=440 \
+    debug.sf.hw=1 \
+    dev.pm.dyn_samplingrate=1 \
+    debug.hwui.use_buffer_age=false \
+    sdm.debug.rotator_downscale=1
+
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.image-dex2oat-filter=speed \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.sys.fw.dex2oat_thread_count=8 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-threads=8
+
 # Display features
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.displayfeature.histogram.enable=false \
