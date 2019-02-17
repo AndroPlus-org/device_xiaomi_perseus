6438cf28fb832d491bf78b033c7e4e08f0eeaa77
AndroPlus <mail@andro.plus>
2019-02-17 19:26:35 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# android_device_xiaomi_perseus
+For building TWRP for Xiaomi Mi MIX 3
+
+TWRP device tree for Xiaomi MIX 3
+
+The Xiaomi Mi MIX 3 (codenamed _"perseus"_) is a high-end smartphone from Xiaomi.
+
+Xiaomi Mi MIX 3 was announced on the 25th of October 2018 and released on the 1st of November 2018.
+
+
+## Device picture
+
+![Xiaomi Mi MIX 3](https://i.imgur.com/dLpqgDx.jpg)
+
+## Thanks
+
+- @travismills82 for his TWRP tree used as skeleton: [twrp_android_device_samsung_star2qltechn](https://github.com/travismills82/twrp_android_device_samsung_star2qltechn)
+
+- @TeamWin for sagit TWRP tree used for partial decryption works: [android_device_xiaomi_sagit](https://github.com/TeamWin/android_device_xiaomi_sagit)
+
+- @timesleader for dipper TWRP tree used for adb works:[android_device_xiaomi_dipper](https://github.com/timesleader/android_device_xiaomi_dipper)
+
+- @wuxianlin for enchilada TWRP tree used for init.rc works:[android_device_oneplus_enchilada](https://github.com/TeamWin/android_device_oneplus_enchilada)
---
+++ omni_perseus.mk
@@ -0,0 +1,37 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := perseus
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_perseus
+PRODUCT_DEVICE := perseus
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI MIX 3
+PRODUCT_MANUFACTURER := Xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := perseus
+TARGET_VENDOR_DEVICE_NAME := perseus
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=perseus \
+    BUILD_PRODUCT=perseus \
+    PRODUCT_NAME=perseus
